4adf27fe6bdb80996fdbe052d858ae2313b9ed3a
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2024-05-04 18:33:21 -0300

    chore: Update index page layout with centered header and links

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -3,16 +3,22 @@ from link_bio.components.navbar import navbar
 from link_bio.views.header.header import header
 from link_bio.views.links.links import links
 from link_bio.components.footer import footer
+from link_bio.styles.styles import styles
 class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.vstack(
+    return rx.box(
         navbar(),
-        header(),
-        links(),
+        rx.center(
+            rx.vstack(
+            header(),
+            links(),
+            max_width="600px",
+            width="100%"      
+            )
+        ),
         footer()
-    
     )
 
 
---
+++ link_bio/link_bio/styles/styles.py
@@ -0,0 +1,7 @@
+import reflex as rx
+from enum import Enum
+
+# Constants
+MAX_WIDTH = "560px"
+FADEIN_ANIMATION = "animate__animated animate__fadeIn"
+BOUNCEIN_ANIMATION = "animate__animated animate__bounceIn"
