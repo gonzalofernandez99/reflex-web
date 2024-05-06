b1b8086d73ab451198fa0563fb97ce953d702897
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2024-05-05 21:04:58 -0300

    chore: Update index page with new links and titles

--- link_bio/link_bio/views/links/links.py
+++ link_bio/link_bio/views/links/links.py
@@ -1,9 +1,34 @@
 import reflex as rx
+import link_bio.constants as const
+from link_bio.routes import Route
 from link_bio.components.link_button import link_button
-def links() -> rx.Component:
+from link_bio.components.title import title
+from link_bio.styles.styles import Color, Spacing
+from link_bio.state.PageState import PageState
+
+
+def index_links() -> rx.Component:
     return rx.vstack(
-        link_button(),
-        link_button(),
-        link_button(),
-        link_button()
+        title("Yotube"),
+        link_button(
+            "YouTube",
+            "Tutoriales sobre desarrollo de software semanales",
+            "/icons/youtube.svg",
+            const.YOUTUBE_URL
+        ),
+
+        title("Linkedin"),
+        link_button(
+            "Linkedin - Contacto",
+            "Aquí puedes ver los lugares en donde estoy trabajando",
+            "/icons/git.svg",
+            const.LINKEDIN_URL
+        ),
+        
+        link_button(
+            "Email",
+            const.EMAIL,
+            "/icons/email.svg",
+            f"mailto:{const.EMAIL}"
+        )
     )
